3cc2b67dab0dd044079ba78f107a52260c33f505
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2022-11-24 23:30:06 +0530

    Update readcsv.js

--- json-transformation-server/src/readcsv/readcsv.js
+++ json-transformation-server/src/readcsv/readcsv.js
@@ -5,6 +5,7 @@ const {NumberAddition,
   shift}=require('../utils/TransformUtils');
 const fs = require('fs');
 const { type } = require('os');
+const { bool } = require('joi');
 const results = [];
 const sourceJSON={
   "id": "122-34-6543",
@@ -22,11 +23,99 @@ const sourceJSON={
 const targetJSON={}
 let specJSONString="{";
 
-fs.createReadStream('../../../data/sample_1/mapping.csv')
+fs.createReadStream('../../../data/sample_2/mapping.csv')
   .pipe(csv())
-  .on('data', (data) => results.push(data))
+  .on('data', (data) =>{
+    let str = JSON.stringify(data);
+    if(str.includes("ENUM")){
+
+      const x={}
+      let enumerate = "";
+      // console.log(data);
+      for(let y in data){
+        // console.log(y,data[y]);
+        if(y==='No.' || y==="Target"){
+          x[y] = data[y]
+          continue;
+        }else{
+          if(y===" Source"){
+            var i = data[y].indexOf(',');
+            var source = data[y].slice(0,i);
+            // console.log(data[y],source);
+            // console.log(i,data[y].length-1);
+            if(i===-1){
+              // console.log("hu");
+              x[" Source"]=data[y];
+            }else{
+              x[" Source"]=source;
+            }
+
+            if(!(i===-1)){
+              enumerate += data[y].slice(i+1)+",";
+              // console.log("ppm",enumerate);
+            }
+
+          }
+          else{
+            enumerate+=data[y]+",";
+          }
+        }
+      }
+      // console.log("enuma",enumerate);
+      let st ={};
+      if(enumerate.length!==0){
+        // console.log(enumerate);
+        let ap = enumerate.split(",");
+        // console.log(ap);
+        for(let k=0;k<ap.length;k++){
+          var po = ap[k];
+          let key="";
+          let val="";
+          let bool = 1;
+          for(let m=0;m<po.length;m++){
+            if(po[m]==="}"){
+              break;
+            }
+            if(po[m]==="\"" || po[m]==="{"){
+              continue;
+            }
+            else{
+              if(po[m]==":"){
+                bool=0;
+                continue;
+              }
+              if(bool===1){
+                key+=po[m];
+              }else{
+                val+=po[m]
+              }
+            }
+          }
+          if(key===""){
+            continue;
+          }
+          st[key.trim()] = val.trim();
+
+        }
+      }
+      // console.log(st);
+      x[" Enumeration"] = st;
+      results.push(x);
+      // console.log(x);
+      // console.log(arr);
+
+      // for(let i=0;i<arr.length;i++){
+      //   let a = arr[i];
+
+      // }
+    }else{
+      results.push(data);
+    }
+
+  }
+  )
   .on('end', () => {
-    // console.log(results);
+    console.log(results);
     for(let i=0 ;i<results.length;i++){
         let value = results[i][" Source"];
         if(!value.includes('(')){
@@ -57,7 +146,7 @@ fs.createReadStream('../../../data/sample_1/mapping.csv')
         // console.log(specJSONString);
 
     }else if(value.includes('ENUM')){
-      
+
       let enum_key = value.match("(?<=\.|^)[^.]+$")[0].slice(0,-1);
 
       let enum_original_value = sourceJSON[enum_key];
